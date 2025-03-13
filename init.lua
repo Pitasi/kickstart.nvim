8ba858edbb5d4207fa3a788db9cc5263b447cb4f
Antonio Pitasi <antonio@pitasi.dev>
2025-03-13 11:19:17 +0100

    avoid useless lazydev types imports

--- init.lua
+++ init.lua
@@ -472,7 +472,6 @@ require('lazy').setup({
       library = {
         -- Load luvit types when the `vim.uv` word is found
         { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
-        vim.fn.stdpath 'data' .. '/lazy',
       },
     },
   },
